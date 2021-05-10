034e1746f0665349c722c255d9b2cac3bc085b51
Kunal Patwardhan <kunal.patwardhan@gmail.com>
2021-05-11 00:10:05 +0530

    basic implementation

--- WinFormsApp2/CowinNotifier.Designer.cs
+++ WinFormsApp2/CowinNotifier.Designer.cs
@@ -29,20 +29,44 @@ namespace WinFormsApp2
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.lstIndex = new System.Windows.Forms.ListView();
+            this.txtCenterMatched = new System.Windows.Forms.TextBox();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(645, 253);
+            this.button1.Location = new System.Drawing.Point(3, 3);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 0;
@@ -57,11 +81,11 @@ namespace WinFormsApp2
             this.Column1,
             this.Column2});
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(122, 125);
+            this.dataGridView1.Size = new System.Drawing.Size(801, 643);
             this.dataGridView1.TabIndex = 1;
             // 
             // Column1
@@ -80,29 +104,174 @@ namespace WinFormsApp2
             // 
             // splitContainer1
             // 
-            this.splitContainer1.Location = new System.Drawing.Point(123, 98);
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
             this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
             // 
             // splitContainer1.Panel2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
-            this.splitContainer1.Size = new System.Drawing.Size(188, 125);
-            this.splitContainer1.SplitterDistance = 62;
+            this.splitContainer1.Panel2.Controls.Add(this.tabControl1);
+            this.splitContainer1.Size = new System.Drawing.Size(1219, 682);
+            this.splitContainer1.SplitterDistance = 400;
             this.splitContainer1.TabIndex = 2;
             // 
-            // Form1
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.lstIndex);
+            this.splitContainer2.Panel1.Controls.Add(this.txtCenterMatched);
+            this.splitContainer2.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.splitContainer3);
+            this.splitContainer2.Size = new System.Drawing.Size(400, 682);
+            this.splitContainer2.SplitterDistance = 337;
+            this.splitContainer2.TabIndex = 1;
+            // 
+            // lstIndex
+            // 
+            this.lstIndex.HideSelection = false;
+            this.lstIndex.Location = new System.Drawing.Point(12, 47);
+            this.lstIndex.Name = "lstIndex";
+            this.lstIndex.Size = new System.Drawing.Size(364, 265);
+            this.lstIndex.TabIndex = 2;
+            this.lstIndex.UseCompatibleStateImageBehavior = false;
+            this.lstIndex.SelectedIndexChanged += new System.EventHandler(this.lstIndex_SelectedIndexChanged);
+            // 
+            // txtCenterMatched
+            // 
+            this.txtCenterMatched.Location = new System.Drawing.Point(153, 4);
+            this.txtCenterMatched.Name = "txtCenterMatched";
+            this.txtCenterMatched.Size = new System.Drawing.Size(125, 27);
+            this.txtCenterMatched.TabIndex = 1;
+            this.txtCenterMatched.TextChanged += new System.EventHandler(this.txtCenterMatched_TextChanged);
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.txtKey);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.txtValue);
+            this.splitContainer3.Size = new System.Drawing.Size(400, 341);
+            this.splitContainer3.SplitterDistance = 200;
+            this.splitContainer3.TabIndex = 0;
+            // 
+            // txtKey
+            // 
+            this.txtKey.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtKey.Location = new System.Drawing.Point(0, 0);
+            this.txtKey.Multiline = true;
+            this.txtKey.Name = "txtKey";
+            this.txtKey.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtKey.Size = new System.Drawing.Size(200, 341);
+            this.txtKey.TabIndex = 1;
+            // 
+            // txtValue
+            // 
+            this.txtValue.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtValue.Location = new System.Drawing.Point(0, 0);
+            this.txtValue.Multiline = true;
+            this.txtValue.Name = "txtValue";
+            this.txtValue.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtValue.Size = new System.Drawing.Size(196, 341);
+            this.txtValue.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(815, 682);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(807, 649);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(807, 649);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(801, 643);
+            this.textBox1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // CowinNotifier
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1219, 682);
             this.Controls.Add(this.splitContainer1);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "CowinNotifier";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CowinNotifier_FormClosing);
+            this.Load += new System.EventHandler(this.CowinNotifier_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel1.PerformLayout();
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            this.splitContainer3.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -114,6 +283,17 @@ namespace WinFormsApp2
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.TextBox txtCenterMatched;
+        private System.Windows.Forms.ListView lstIndex;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- WinFormsApp2/CowinNotifier.cs
+++ WinFormsApp2/CowinNotifier.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using FormSerialisation;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -24,7 +25,7 @@ namespace WinFormsApp2
         {
             //WebRequest request = WebRequest.Create("https://www.cowin.gov.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=363&date=11-05-2021");
             //WebResponse response = request.GetResponse();
-
+            timer1.Start();
 
             string urlToCheck = "";
             HttpWebRequest request = HttpWebRequest.Create("https://www.cowin.gov.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=363&date=11-05-2021") as HttpWebRequest;
@@ -43,21 +44,112 @@ namespace WinFormsApp2
             using (StreamReader r = new StreamReader(response.GetResponseStream()))
             {
                 string json = r.ReadToEnd();
+                textBox1.Text = json;
+                textBox1.Text = textBox1.Text.Replace("center_id", "center_id" + Environment.NewLine);
 
                 string[] splitJSON = json.Split(",");
 
-                for(int i=0;i<splitJSON.Count();i++)
+                string[] strArrKey = txtKey.Text.Split(Environment.NewLine);
+                string[] strArrValue = txtValue.Text.Split(Environment.NewLine);
+
+                int centerMatchedCount = 0;
+                int propertyMatchedCount = 0;
+                string lastMatched = "";
+
+                if (splitJSON.Count() > 0)
                 {
-                    dataGridView1.RowCount++;
-                    string[] split2 = splitJSON[i].Split(":");
-                    if (split2.Length == 2)
+                    dataGridView1.RowCount = splitJSON.Count();
+                    lstIndex.Clear();
+                    for (int i = 0; i < splitJSON.Count(); i++)
                     {
-                        dataGridView1[0, i].Value = split2[0];
-                        dataGridView1[1, i].Value = split2[1];
+                        //dataGridView1.RowCount++;
+                        string[] split2 = splitJSON[i].Split(":");
+                        if (split2.Length == 2)
+                        {
+                            dataGridView1[0, i].Value = split2[0];
+                            dataGridView1[1, i].Value = split2[1];
+
+                            if (split2[0] == "\"name\"")
+                            {
+                                propertyMatchedCount = 0;
+                                lastMatched = "";
+                            }
+
+                            if (split2[0] == "{\"session_id\"")
+                            {
+                                propertyMatchedCount = 0;
+                                lastMatched = "";
+                            }
+
+                            for (int j = 0; j < strArrKey.Length; j++)
+                            {
+                                string keyStr = strArrKey[j];
+                                string ValueStr = strArrValue[j];
+                                if (ValueStr.Contains('!'))
+                                {
+                                    if ((keyStr == split2[0]) && (ValueStr != ('!' + split2[1])))
+                                    {
+                                        if (lastMatched != keyStr)
+                                            propertyMatchedCount++;
+
+                                        lastMatched = keyStr;
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    if ((keyStr == split2[0]) && (ValueStr == split2[1]))
+                                    {
+                                        if (lastMatched != keyStr)
+                                            propertyMatchedCount++;
+
+                                        lastMatched = keyStr;
+                                        break;
+                                    }
+                                }
+                            }
+
+                            if (propertyMatchedCount == strArrKey.Length)
+                            {
+                                centerMatchedCount++;
+                                propertyMatchedCount = 0;
+                                lastMatched = "";
+                                dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.Red;
+                                lstIndex.Items.Add(i.ToString());
+                            }
+
+                        }
+
                     }
+                    txtCenterMatched.Text = centerMatchedCount.ToString();
                 }
-                
             }
+        }
+
+        private void CowinNotifier_Load(object sender, EventArgs e)
+        {
+            FormSerialisor.Deserialise(this, Application.StartupPath + @"\serialise.xml");
+        }
+
+        private void CowinNotifier_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            FormSerialisor.Serialise(this, Application.StartupPath + @"\serialise.xml");
+        }
+
+        private void lstIndex_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView1.FirstDisplayedScrollingRowIndex = Convert.ToInt32( lstIndex.FocusedItem.Text);
+        }
+
+        private void txtCenterMatched_TextChanged(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(WindowState==FormWindowState.Minimized)
+                button1_Click(null, null);
         }
     }
 }
